d870681a1f33535e5395a1fe20a9205a744370d6
timhildebrandt <17_Tim_H@web.de>
2018-03-28 15:13:17 +0200

    uebung01_loesungsvorschlag

--- src/main/java/ueb01/CharStackImpl.java
+++ src/main/java/ueb01/CharStackImpl.java
@@ -1,4 +1,38 @@
 package ueb01;
 
+import java.util.NoSuchElementException;
+
 class CharStackImpl implements CharStack {
+    private char[] stack;
+    public CharStackImpl() {
+        stack = new char[0];
+    } //leer.
+
+    @Override
+    public void push(char c) {
+        char[] neu = new char[stack.length + 1];
+        System.arraycopy(stack, 0, neu, 0, stack.length);
+        neu[stack.length] = c;
+        stack = neu;
+    }
+
+    @Override
+    public char pop() {
+        char c;
+        if (stack.length == 0) {
+            throw new NoSuchElementException();
+        } else {
+            char[] neu = new char[stack.length - 1];
+            c = stack[stack.length -1];
+            System.arraycopy(stack, 0, neu, 0, neu.length);
+            stack = neu;
+        }
+
+        return c;
+    }
+
+    @Override
+    public int size() {
+        return stack.length;
+    }
 }
